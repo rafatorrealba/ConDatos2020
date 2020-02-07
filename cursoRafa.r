373959752cd561ae1162ed6fab42bace70c8efbd
rafatorrealba <47217363+rafatorrealba@users.noreply.github.com>
2020-02-07 15:39:51 -0400

    Delete cursoRafa.r

--- cursoRafa.r
+++
@@ -1,9 +0,0 @@
-#librerias del curso de r.
-library(tidyverse)
-  library(dslabs)
-  data(murders)
-  
-  #indice de asesinatos en las ciudades de los estados unidos
-murders %>%
-    ggplot(aes(population, total, label = abb, color = region)) +
-    geom_label()
